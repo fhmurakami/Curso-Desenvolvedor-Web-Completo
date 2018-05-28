ed1adbae2ed3dbbc0cbd688ac2569644376149b1
Felipe Murakami <fhmurakami@gmail.com>
2018-05-28 16:53:04 -0300

    Add PHP - Unidade04

---
+++ 09-PHP/unidade_04/inicial.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Curso PHP FUNDAMENTAL</title>
+    </head>
+
+    <body>
+        <p><?php 
+            
+            // Comentário de uma linha
+            
+            /*  Comentário 
+                de várias
+                linhas */
+            echo "Felipe Murakami"
+            ?>
+        </p>
+    </body>
+</html>
